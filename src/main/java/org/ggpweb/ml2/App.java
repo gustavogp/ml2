62c8891edc43adfa9126dd57299fe55f42f48356
gustavogp <gustavogpinheiro@gmail.com>
2015-09-22 01:21:11 -0500

    initial commit

---
+++ src/main/java/org/ggpweb/ml2/App.java
@@ -0,0 +1,13 @@
+package org.ggpweb.ml2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
